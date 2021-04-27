275125901fff590e2d533947de9cecd80d670ddf
Chen Sihan <schen@lcs.on.ca>
2021-04-26 23:28:12 -0400

    Initial Commit

---
+++ HighschoolCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HighschoolCalculator
+//
+//  Created by Chen, Sihan on 2021-04-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HighschoolCalculator/HighschoolCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighschoolCalculatorApp.swift
+//  HighschoolCalculator
+//
+//  Created by Chen, Sihan on 2021-04-26.
+//
+
+import SwiftUI
+
+@main
+struct HighschoolCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
